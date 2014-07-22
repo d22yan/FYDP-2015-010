1b0713e69009777f40a14862ec1ccf6cc48bed06
Lewis Liu <develop@lewisl.net>
2014-07-22 18:49:35 -0400

    -added workaround for time sync issues

--- client/app/filters/unixToTimeAgo/unixToTimeAgo.filter.js
+++ client/app/filters/unixToTimeAgo/unixToTimeAgo.filter.js
@@ -3,6 +3,6 @@
 angular.module('dtmsgApp')
   .filter('unixToTimeAgo', function (Time) {
     return function (unixTime) {
-      return unixTime ? Time.fromNow().replace('a few ', '') : 'Never';
+      return unixTime ? Time.fromNow().replace('in a few seconds', 'just now').replace('a few ', '') : 'never';
     };
   });
