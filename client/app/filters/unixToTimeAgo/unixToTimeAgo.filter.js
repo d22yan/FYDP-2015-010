d46aa7e191dfb23e2506a7f49a54a69702e7bcec
Lewis Liu <develop@lewisl.net>
2014-07-22 21:55:36 -0400

    -fixed always present scroll bar -changed 'just now' time message for time difference sync to 'seconds ago'

--- client/app/filters/unixToTimeAgo/unixToTimeAgo.filter.js
+++ client/app/filters/unixToTimeAgo/unixToTimeAgo.filter.js
@@ -3,6 +3,6 @@
 angular.module('dtmsgApp')
   .filter('unixToTimeAgo', function (Time) {
     return function (unixTime) {
-      return unixTime ? Time.fromNow(unixTime).replace('in a few seconds', 'just now').replace('a few ', '') : 'never';
+      return unixTime ? Time.fromNow(unixTime).replace('in a few seconds', 'seconds ago').replace('a few ', '') : 'never';
     };
   });
