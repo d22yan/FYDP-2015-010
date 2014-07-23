70edf23f67d3c1e7e0dd41b3c20f26e9372bb0fe
sanghoon <sh42lee@uwaterloo.ca>
2014-07-23 18:57:55 -0400

    invite accept reject fixed!

--- client/app/services/communication/communication.service.js
+++ client/app/services/communication/communication.service.js
@@ -72,6 +72,7 @@ angular.module('dtmsgApp')
             if (existingContact) {
               if(existingContact.status === Constants.userStatus.invited) {
                 this.initializeContact(existingContact);
+                existingContact.name = packet.js.i;
               }
               return $log.info('received invite from existing contact: ' + packet.from.hashname);
             }
